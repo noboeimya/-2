d0ccf9f13d1d83e442e2a9863110babe56740f3a
АЛЕКСАНДР <115793799+noboeimya@users.noreply.github.com>
2022-11-20 21:01:51 +0300

    Add files via upload

---
+++ 036.cs
@@ -0,0 +1,33 @@
+﻿/*Задача 36: 
+Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов,
+ стоящих на нечётных позициях.*/
+
+int[] Finding_Of_Num(int size, int LevtValue, int RightValue)
+{
+    int[] vector = new int[size];
+    for (int i = 1; i < size; i++)
+    {
+        vector[i] = new Random().Next(LevtValue, RightValue + 1);
+    }
+    return vector;
+}
+
+int AdditionElement(int[] array)
+{
+    int sum = 0;
+    for (int i = 1; i < array.Length; i++)
+    {
+        sum += array[i];
+    }
+    return sum;
+}
+Console.Clear();
+Console.Write("вводим размер массива: ");
+int number = int.Parse(Console.ReadLine()!);
+Console.Write("вводим значение левого конца диапазона: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.Write("вводим значение правого конца диапазона: ");
+int b = int.Parse(Console.ReadLine()!);
+int[] WorkArray = Finding_Of_Num(number, a, b);
+Console.WriteLine($"получили массив: [{String.Join(",", WorkArray)}]");
+Console.WriteLine($"ссумма нечетных элементов массива ровна: {AdditionElement(WorkArray)}");
---
+++ 38.cs
@@ -0,0 +1,51 @@
+﻿
+//Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+double[] ReturnArray(int size, int left, int right)
+{
+    double[] numb = new double[size];
+    for (int i = 0; i < size; i++)
+    {
+        numb[i] = Math.Round(new Random().NextDouble() * (right - left) + left, 2);
+    }
+    return numb;
+}
+
+double Maximum(double[] array)
+{
+    double max = array[0];
+    for (int i = 1; i < array.Length; i++)
+    {
+        if (array[i] > max)
+        {
+            max = array[i];
+        }
+    }
+    return max;
+}
+double Minimum(double[] array)
+{
+    double min = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] < min)
+        {
+            min = array[i];
+        }
+    }
+    return min;
+}
+Console.Write("введите размер массива: ");
+int number = int.Parse(Console.ReadLine()!);
+Console.Write("введите конец левого диапазона: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.Write("введите конец правого диапазона: ");
+int b = int.Parse(Console.ReadLine()!);
+double[] WorkArray = ReturnArray(number, a, b);
+Console.WriteLine($" получили массив: [{String.Join(", ", WorkArray)}]");
+
+Console.WriteLine($" наименьший массив: {Minimum(WorkArray)}");
+Console.WriteLine($" наибольший массив: {Maximum(WorkArray)}");
+
+double diference = Maximum(WorkArray) - Minimum(WorkArray);
+Console.WriteLine($"разницу между максимальным и минимальным элементов массива составило:  {diference}");
