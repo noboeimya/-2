f5753c2e709c1987583eef4386aa70cc9aa53ddd
АЛЕКСАНДР <115793799+noboeimya@users.noreply.github.com>
2022-11-24 17:19:38 +0300

    Add files via upload

---
+++ 52.cs
@@ -0,0 +1,44 @@
+﻿//задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+int[,] GetArray(int num1, int num2, int min, int max)
+{
+    Random rnd = new Random();
+    int[,] newArray = new int[num1, num2];
+    for (int i = 0; i < num1; i++)
+    {
+        for (int j = 0; j < num2; j++)
+        {
+            newArray[i, j] = rnd.Next(min, max + 1);
+        }
+
+    }
+    return newArray;
+}
+void PrintIntArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.WriteLine($" {inArray[i, j],5}");
+        }
+    }
+}
+void ColumnArray(int[,] array)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        int summ = 0;
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            summ += array[i, j];
+        }
+        summ /= array.GetLength(0);
+        Console.Write($" {summ,5} ");
+    }
+}
+int[,] myArray = GetArray(3, 3, -4, 4);
+PrintIntArray(myArray);
+Console.WriteLine("--------------"); 
+
+ColumnArray(myArray);
---
+++ звездочка 7.cs
@@ -0,0 +1,52 @@
+﻿/*Задача со звездочкой: Написать программу для перевода римских чисел в десятичные арабские.
+III -> 3
+LVIII -> 58
+MCMXCIV -> 1994
+*/
+
+int RomeToAracbic(char sumbol)
+{   
+    string romeNumeric = "IVXLCDM";
+    int[] arabicNumeric = new int[7] { 1, 5, 10, 50, 100, 500, 1000 };
+    int index = romeNumeric.IndexOf(sumbol);
+    if (index >= 0) return arabicNumeric[index];
+    return 0;
+}
+
+int RomeNumberParse(string romeNumber)
+{
+    int result = 0;
+    for (int i = 0; i < romeNumber.Length; i++)
+    {
+        int currentNumber = RomeToAracbic(romeNumber[i]);
+        if (currentNumber == 0)
+        {  
+            throw new Exception("Недопустимый символ в римской записи числа!.");
+        }
+        if (i + 1 < romeNumber.Length)
+        {
+            
+            if (currentNumber < RomeToAracbic(romeNumber[i + 1]))
+            {
+                result -= currentNumber;
+            }
+            else
+            {
+                result += currentNumber;
+            }
+        }
+        else
+    
+        {
+            result += currentNumber;
+        }
+    }
+    return result;
+}
+
+string[] exampleRome = { "III", "LVIII", "MCMXCIV" };
+foreach (var item in exampleRome)
+{
+    int arabicNumber = RomeNumberParse(item);
+    Console.WriteLine($"{item} = {arabicNumber}");
+}
